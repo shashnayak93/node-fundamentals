18a28090995c479f8ea626efd0920366d6900c38
Shashwath <shash.nayak93@gmail.com>
2023-11-15 11:32:53 -0500

    os & path modules

---
+++ 3-modules.js
@@ -0,0 +1,10 @@
+
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
+require('./7-mind-grenade')
+
+// sayHi(`Shash`)
+// sayHi(names.devi)
+// sayHi(names.bryan)
+// console.log(data)
